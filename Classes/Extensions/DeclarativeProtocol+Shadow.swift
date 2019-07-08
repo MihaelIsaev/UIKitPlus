2891516f0cb5de3be1f9e297785f3f108a17aaa3
Mihael Isaev <isaev.mihael@gmail.com>
2019-07-08 20:12:15 +0400

    Improve `shadow` extension

--- Classes/Extensions/DeclarativeProtocol+Shadow.swift
+++ Classes/Extensions/DeclarativeProtocol+Shadow.swift
@@ -2,16 +2,16 @@ import UIKit
 
 extension DeclarativeProtocol {
     @discardableResult
-    public func shadow(_ color: UIColor = .black, opacity: Float = 1, offset: CGSize = .zero, radius: CGFloat = 10) -> Self {
+    public func shadow(_ color: UIColor = .black, opacity: Float = 1, x: CGFloat = 0, y: CGFloat = 0, radius: CGFloat = 10) -> Self {
         declarativeView.layer.shadowColor = color.cgColor
         declarativeView.layer.shadowOpacity = opacity
-        declarativeView.layer.shadowOffset = offset
+        declarativeView.layer.shadowOffset = CGSize(width: x, height: y)
         declarativeView.layer.shadowRadius = radius
         return self
     }
     
     @discardableResult
-    public func shadow(_ colorNumber: Int, opacity: Float = 1, offset: CGSize = .zero, radius: CGFloat = 10) -> Self {
-        return shadow(colorNumber.color, opacity: opacity, offset: offset, radius: radius)
+    public func shadow(_ colorNumber: Int, opacity: Float = 1, x: CGFloat = 0, y: CGFloat = 0, radius: CGFloat = 10) -> Self {
+        return shadow(colorNumber.color, opacity: opacity, x: x, y: y, radius: radius)
     }
 }
