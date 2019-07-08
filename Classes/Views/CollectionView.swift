99eb66c758f5bc4a0bb35773ffac2b5ac679fb3c
Mihael Isaev <isaev.mihael@gmail.com>
2019-07-08 20:20:01 +0400

    Improve `CollectionViewCell`

--- Classes/Views/CollectionView.swift
+++ Classes/Views/CollectionView.swift
@@ -11,13 +11,21 @@ open class CollectionView: UICollectionView, DeclarativeProtocol, DeclarativePro
     var _constraintsMain: DeclarativeConstraintsCollection = [:]
     var _constraintsOuter: DeclarativeConstraintsKeyValueCollection = [:]
     
-    public init (_ layout: UICollectionViewLayout = UICollectionViewFlowLayout()) {
-        super.init(frame: .zero, collectionViewLayout: layout)
-        translatesAutoresizingMaskIntoConstraints = false
+    static var defaultLayout: UICollectionViewFlowLayout {
+        return CollectionViewFlowLayout().itemSize(50).minimumInteritemSpacing(5).minimumLineSpacing(5)
+    }
+    
+    public convenience init (_ layout: UICollectionViewLayout) {
+        self.init(frame: .zero, collectionViewLayout: layout)
+    }
+    
+    public convenience init () {
+        self.init(frame: .zero, collectionViewLayout: CollectionView.defaultLayout)
     }
     
     public override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
         super.init(frame: frame, collectionViewLayout: layout)
+        translatesAutoresizingMaskIntoConstraints = false
     }
     
     required public init?(coder aDecoder: NSCoder) {
@@ -37,8 +45,81 @@ open class CollectionView: UICollectionView, DeclarativeProtocol, DeclarativePro
     // MARK: Paging
     
     @discardableResult
-    public func paging(_ enabled: Bool) -> CollectionView {
+    public func paging(_ enabled: Bool) -> Self {
         isPagingEnabled = enabled
+        
+        return self
+    }
+    
+    // MARK: Scrolling
+    
+    @discardableResult
+    public func scrolling(_ enabled: Bool) -> Self {
+        isScrollEnabled = enabled
+        return self
+    }
+    
+    // MARK: Indicators
+    
+    @discardableResult
+    public func hideIndicator(_ indicators: NSLayoutConstraint.Axis...) -> Self {
+        if indicators.contains(.horizontal) {
+            showsHorizontalScrollIndicator = false
+        }
+        if indicators.contains(.vertical) {
+            showsVerticalScrollIndicator = false
+        }
+        return self
+    }
+    
+    // MARK: Indicators
+    
+    @discardableResult
+    public func hideAllIndicators() -> Self {
+        showsHorizontalScrollIndicator = false
+        showsVerticalScrollIndicator = false
+        return self
+    }
+    
+    // MARK: Content Inset
+    
+    @discardableResult
+    public func contentInset(_ insets: UIEdgeInsets) -> Self {
+        contentInset = insets
+        return self
+    }
+    
+    @discardableResult
+    public func contentInset(top: CGFloat = 0, left: CGFloat = 0, right: CGFloat = 0, bottom: CGFloat = 0) -> Self {
+        return contentInset(.init(top: top, left: left, bottom: bottom, right: right))
+    }
+    
+    // MARK: Scroll Indicator Inset
+    
+    @discardableResult
+    public func scrollIndicatorInsets(_ insets: UIEdgeInsets) -> Self {
+        scrollIndicatorInsets = insets
+        return self
+    }
+    
+    @discardableResult
+    public func scrollIndicatorInsets(top: CGFloat = 0, left: CGFloat = 0, bottom: CGFloat = 0, right: CGFloat = 0) -> Self {
+        return scrollIndicatorInsets(.init(top: top, left: left, bottom: bottom, right: right))
+    }
+    
+    // MARK: Delegate
+    
+    @discardableResult
+    public func delegate(_ delegate: UICollectionViewDelegate) -> Self {
+        self.delegate = delegate
+        return self
+    }
+    
+    // MARK: Delegate
+    
+    @discardableResult
+    public func dataSource(_ dataSource: UICollectionViewDataSource) -> Self {
+        self.dataSource = dataSource
         return self
     }
 }
