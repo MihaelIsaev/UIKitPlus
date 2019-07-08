8eeeac372c6eb2f3b900e98db955d530c0ffe435
Mihael Isaev <isaev.mihael@gmail.com>
2019-07-08 20:16:42 +0400

    Improve `View`

--- Classes/Views/View.swift
+++ Classes/Views/View.swift
@@ -34,6 +34,77 @@ open class View: UIView, DeclarativeProtocol, DeclarativeProtocolInternal {
         super.didMoveToSuperview()
         movedToSuperview()
     }
+    
+    open func buildView() {}
+    
+    // MARK: Touches
+    
+    public typealias TouchClosure = (Set<UITouch>, UIEvent?) -> Void
+    
+    private var _touchesBegan: TouchClosure?
+    private var _touchesMoved: TouchClosure?
+    private var _touchesEnded: TouchClosure?
+    private var _touchesCancelled: TouchClosure?
+    
+    @discardableResult
+    public func touchesBegan(_ closure: @escaping TouchClosure) -> Self {
+        _touchesBegan = closure
+        return self
+    }
+    
+    @discardableResult
+    public func touchesMoved(_ closure: @escaping TouchClosure) -> Self {
+        _touchesBegan = closure
+        return self
+    }
+    
+    @discardableResult
+    public func touchesEnded(_ closure: @escaping TouchClosure) -> Self {
+        _touchesBegan = closure
+        return self
+    }
+    
+    @discardableResult
+    public func touchesCancelled(_ closure: @escaping TouchClosure) -> Self {
+        _touchesBegan = closure
+        return self
+    }
+    
+    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        _touchesBegan?(touches, event)
+    }
+    
+    open override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        _touchesMoved?(touches, event)
+    }
+    
+    override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        _touchesEnded?(touches, event)
+    }
+    
+    open override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        _touchesCancelled?(touches, event)
+    }
+    
+    // MARK: Single Tap
+    
+    private var _tapAction: ()->Void = {}
+    
+    @discardableResult
+    public func tapAction(_ action: @escaping ()->Void) -> Self {
+        _tapAction = action
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tap)))
+        return self
+    }
+    
+    @objc
+    private func tap() {
+        _tapAction()
+    }
 }
 
 // MARK: Convenience Initializers
