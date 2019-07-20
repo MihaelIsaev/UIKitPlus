7f37cb4cb264568b212e5c96ef6ea92d6d9b3bc7
Mihael Isaev <isaev.mihael@gmail.com>
2019-07-20 14:27:54 +0400

    Implement `ImpactFeedback`

---
+++ Classes/Objects/ImpactFeedback.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+public class ImpactFeedback {
+    public enum FeedbackStyle: Int {
+        case light, medium, heavy
+    }
+    
+    public static func bzz(_ style: FeedbackStyle = .light) {
+        if #available(iOS 10.0, *) {
+            guard let style = UIImpactFeedbackGenerator.FeedbackStyle(rawValue: style.rawValue) else { return }
+            UIImpactFeedbackGenerator(style: style).impactOccurred()
+        }
+    }
+    
+    public static func selected() {
+        if #available(iOS 10.0, *) {
+            UISelectionFeedbackGenerator().selectionChanged()
+        }
+    }
+    
+    public static func success() {
+        if #available(iOS 10.0, *) {
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+        }
+    }
+    
+    public static func warning() {
+        if #available(iOS 10.0, *) {
+            UINotificationFeedbackGenerator().notificationOccurred(.warning)
+        }
+    }
+    
+    public static func error() {
+        if #available(iOS 10.0, *) {
+            UINotificationFeedbackGenerator().notificationOccurred(.error)
+        }
+    }
+}
