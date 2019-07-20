25a7598ff104c7ce9d868e8398fdfbd718f92488
Mihael Isaev <isaev.mihael@gmail.com>
2019-07-20 14:31:22 +0400

    Extend `NavigationController` with animated `push` and `pop` methods

---
+++ Classes/Extensions/NavigationController+FadeTo.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+extension UINavigationController {
+    public func pushViewControllerAnimated(_ viewController: UIViewController, animation: CATransitionType = .fade, duration: CFTimeInterval = 0.3) {
+        let transition: CATransition = CATransition()
+        transition.duration = duration
+        transition.type = animation
+        view.layer.add(transition, forKey: nil)
+        pushViewController(viewController, animated: false)
+    }
+    
+    public func popViewControllerAnimated(animation: CATransitionType = .fade, duration: CFTimeInterval = 0.3) {
+        let transition: CATransition = CATransition()
+        transition.duration = duration
+        transition.type = animation
+        view.layer.add(transition, forKey: nil)
+        popViewController(animated: false)
+    }
+}
