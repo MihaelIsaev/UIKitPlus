88fc9c9d60419f054b9ca045d3c6ab40b5d2ddab
Mihael Isaev <isaev.mihael@gmail.com>
2019-07-20 14:33:06 +0400

    Add `centerX` to `DeclarativeConstraintXSide`

--- Classes/Enums/DeclarativeConstraintXSide.swift
+++ Classes/Enums/DeclarativeConstraintXSide.swift
@@ -1,13 +1,15 @@
 import UIKit
 
 public enum DeclarativeConstraintXSide: Int {
-    case leading, leadingMargin, trailing, trailingMargin
+    case leading, leadingMargin, trailing, trailingMargin, centerX, centerXMargin
     var side: NSLayoutConstraint.Attribute {
         switch self {
         case .leading: return .leading
         case .leadingMargin: return .leadingMargin
         case .trailing: return .trailing
         case .trailingMargin: return .trailingMargin
+        case .centerX: return .centerX
+        case .centerXMargin: return .centerXWithinMargins
         }
     }
 }
