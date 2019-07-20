c75ff1615959090518a4b58cbd2374185bf5b289
Mihael Isaev <isaev.mihael@gmail.com>
2019-07-20 14:31:56 +0400

    Extend `DeclarativeProtocol` with bounds methods

---
+++ Classes/Extensions/DeclarativeProtocol+Bounds.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+extension DeclarativeProtocol {
+    @discardableResult
+    public func clipsToBounds(_ value: Bool = true) -> Self {
+        declarativeView.clipsToBounds = value
+        return self
+    }
+    
+    @discardableResult
+    public func masksToBounds(_ value: Bool = true) -> Self {
+        declarativeView.layer.masksToBounds = value
+        return self
+    }
+}
