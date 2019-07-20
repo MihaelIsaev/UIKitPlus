b51693cd757fa37d7fd3382c61368ffea9863a8c
Mihael Isaev <isaev.mihael@gmail.com>
2019-07-20 15:29:18 +0400

    `ViewController` improve keyboard appearance handlers logic

--- Classes/Controllers/FormViewController.swift
+++ Classes/Controllers/FormViewController.swift
@@ -10,62 +10,16 @@ open class FormViewController: ViewController {
         scrollView.addSubview(stackView)
     }
     
-    public override init() {
-        super.init()
-        subscribeToKeyboardNotifications()
-    }
-    
-    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        subscribeToKeyboardNotifications()
-    }
-    
-    public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self)
-    }
-    
-    private func subscribeToKeyboardNotifications() {
-        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillAppear(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillDisappear(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    var keyboardWasShowedAtLeastOnce = false
-    
-    func keyboardAppeared(_ height: CGFloat, _ animationDuration: TimeInterval) {
-        keyboardWasShowedAtLeastOnce = true
+    override open func keyboardAppeared(_ height: CGFloat, _ animationDuration: TimeInterval, _ inThisController: Bool) {
+        super.keyboardAppeared(height, animationDuration, inThisController)
         scrollView.bottom = height * (-1)
         view.layoutIfNeeded()
     }
     
-    func keyboardDisappeared(_ animationDuration: TimeInterval) -> Bool {
-        guard keyboardWasShowedAtLeastOnce else { return false }
+    override open func keyboardDisappeared(_ animationDuration: TimeInterval, _ inThisController: Bool) -> Bool {
+        guard super.keyboardDisappeared(animationDuration, inThisController) else { return false }
         scrollView.bottom = 0
         view.layoutIfNeeded()
         return true
-    }
-}
-
-// MARK: Keyboard Notifications
-
-extension FormViewController {
-    @objc
-    private func keyboardWillAppear(notification: NSNotification) {
-        guard let userInfo = notification.userInfo else { return }
-        guard let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
-        let keyboardRectangle = keyboardFrame.cgRectValue
-        guard keyboardRectangle.height > 0 else { return }
-        guard let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
-        keyboardAppeared(keyboardRectangle.height, animationDuration)
-    }
-    
-    @objc
-    private func keyboardWillDisappear(notification: NSNotification) {
-        guard let userInfo = notification.userInfo else { return }
-        guard let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
-        _ = keyboardDisappeared(animationDuration)
     }
 }
--- Classes/Controllers/ViewController.swift
+++ Classes/Controllers/ViewController.swift
@@ -1,13 +1,16 @@
+import Foundation
 import UIKit
 
 open class ViewController: UIViewController {
     public init () {
         super.init(nibName: nil, bundle: nil)
+        subscribeToKeyboardNotifications()
         buildUI()
     }
     
     public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        subscribeToKeyboardNotifications()
         buildUI()
     }
     
@@ -15,8 +18,11 @@ open class ViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     open func buildUI() {
-        view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .white
     }
     
@@ -30,5 +36,54 @@ open class ViewController: UIViewController {
         }
     }
     
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        isSubscribedToKeyboardNotifications = true
+    }
+    
+    open override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        isSubscribedToKeyboardNotifications = false
+    }
+    
     open func viewDidAppearFirstTime(_ animated: Bool) {}
+    
+    private var isSubscribedToKeyboardNotifications = false
+    
+    private func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillAppear(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillDisappear(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    var keyboardWasShowedAtLeastOnce = false
+    
+    open func keyboardAppeared(_ height: CGFloat, _ animationDuration: TimeInterval, _ inThisController: Bool) {
+        keyboardWasShowedAtLeastOnce = true
+    }
+    
+    open func keyboardDisappeared(_ animationDuration: TimeInterval, _ inThisController: Bool) -> Bool {
+        guard keyboardWasShowedAtLeastOnce else { return false }
+        return true
+    }
+}
+
+// MARK: Keyboard Notifications
+
+extension ViewController {
+    @objc
+    private func keyboardWillAppear(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        guard let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardRectangle = keyboardFrame.cgRectValue
+        guard keyboardRectangle.height > 0 else { return }
+        guard let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        keyboardAppeared(keyboardRectangle.height, animationDuration, isSubscribedToKeyboardNotifications)
+    }
+    
+    @objc
+    private func keyboardWillDisappear(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        guard let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        _ = keyboardDisappeared(animationDuration, isSubscribedToKeyboardNotifications)
+    }
 }
