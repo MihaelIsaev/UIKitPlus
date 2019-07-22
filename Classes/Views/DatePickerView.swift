53112baa386a930f52d048bd76a2d0946dac4f33
Mihael Isaev <isaev.mihael@gmail.com>
2019-07-22 21:40:22 +0400

    Implement declarative `UIDatePicker`

---
+++ Classes/Views/DatePickerView.swift
@@ -0,0 +1,102 @@
+import UIKit
+
+open class DatePicker: UIDatePicker, DeclarativeProtocol, DeclarativeProtocolInternal {
+    public var declarativeView: DatePicker { return self }
+    
+    var _circleCorners: Bool = false
+    var _customCorners: CustomCorners?
+    lazy var _borders = Borders()
+    
+    var _preConstraints = DeclarativePreConstraints()
+    var _constraintsMain: DeclarativeConstraintsCollection = [:]
+    var _constraintsOuter: DeclarativeConstraintsKeyValueCollection = [:]
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        addTarget(self, action: #selector(changed), for: .valueChanged)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        onLayoutSubviews()
+    }
+    
+    open override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        movedToSuperview()
+    }
+    
+    @discardableResult
+    public func mode(_ value: UIDatePicker.Mode) -> Self {
+        datePickerMode = value
+        return self
+    }
+    
+    @discardableResult
+    public func locale(_ value: Locale) -> Self {
+        locale = value
+        return self
+    }
+    
+    @discardableResult
+    public func calendar(_ value: Calendar) -> Self {
+        calendar = value
+        return self
+    }
+    
+    @discardableResult
+    public func timeZone(_ value: TimeZone) -> Self {
+        timeZone = value
+        return self
+    }
+    
+    @discardableResult
+    public func date(_ value: Date) -> Self {
+        date = value
+        return self
+    }
+    
+    @discardableResult
+    public func minimumDate(_ value: Date) -> Self {
+        minimumDate = value
+        return self
+    }
+    
+    @discardableResult
+    public func maximumDate(_ value: Date) -> Self {
+        maximumDate = value
+        return self
+    }
+    
+    @discardableResult
+    public func countDownDuration(_ value: TimeInterval) -> Self {
+        countDownDuration = value
+        return self
+    }
+    
+    @discardableResult
+    public func minuteInterval(_ value: Int) -> Self {
+        minuteInterval = value
+        return self
+    }
+    
+    // MARK: Handler
+    
+    private var _changed: (Date) -> Void = { _ in }
+    
+    @objc
+    private func changed() {
+        _changed(date)
+    }
+    
+    @discardableResult
+    public func onChange(_ closure: @escaping (Date) -> Void) -> Self {
+        _changed = closure
+        return self
+    }
+}
