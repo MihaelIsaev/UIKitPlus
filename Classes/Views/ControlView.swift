fd9834ab3ab270532ffacd0e0ab7e70c8a1fa119
Mihael Isaev <isaev.mihael@gmail.com>
2019-08-08 07:57:43 +0400

    Implement `ControlView`

---
+++ Classes/Views/ControlView.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+open class ControlView: UIControl, DeclarativeProtocol, DeclarativeProtocolInternal {
+    public var declarativeView: ControlView { return self }
+    
+    var _circleCorners: Bool = false
+    var _customCorners: CustomCorners?
+    lazy var _borders = Borders()
+    
+    var _preConstraints = DeclarativePreConstraints()
+    var _constraintsMain: DeclarativeConstraintsCollection = [:]
+    var _constraintsOuter: DeclarativeConstraintsKeyValueCollection = [:]
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        buildView()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open func buildView() {}
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        onLayoutSubviews()
+    }
+    
+    open override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        movedToSuperview()
+    }
+}
