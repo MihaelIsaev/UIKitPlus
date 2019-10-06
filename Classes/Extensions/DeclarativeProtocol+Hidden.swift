6c50de852369729f2ca50ab8849a7b1b0167d89c
Mihael Isaev <isaev.mihael@gmail.com>
2019-10-07 03:24:05 +0400

    DeclarativeProtocol+Hidden: implement `@State` support

--- Classes/Extensions/DeclarativeProtocol+Hidden.swift
+++ Classes/Extensions/DeclarativeProtocol+Hidden.swift
@@ -6,4 +6,22 @@ extension DeclarativeProtocol {
         declarativeView.isHidden = hidden
         return self
     }
+    
+    @discardableResult
+    public func hidden(_ hidden: State<Bool>) -> Self {
+        declarativeView.isHidden = hidden.wrappedValue
+        hidden.listen { [weak self] old, new in
+            self?.declarativeView.isHidden = new
+        }
+        return self
+    }
+    
+    @discardableResult
+    public func hidden<V>(_ expressable: ExpressableState<V, Bool>) -> Self {
+        declarativeView.isHidden = expressable.value()
+        expressable.state.listen { [weak self] old, new in
+            self?.declarativeView.isHidden = expressable.value()
+        }
+        return self
+    }
 }
