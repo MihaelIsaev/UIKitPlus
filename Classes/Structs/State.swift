e9093fdee1183e67515b3740ae651ae2184f4bd8
Mihael Isaev <isaev.mihael@gmail.com>
2019-10-07 02:25:22 +0400

    Improve `State` by adding extended listener and `originalValue`

--- Classes/Structs/State.swift
+++ Classes/Structs/State.swift
@@ -1,11 +1,13 @@
 @propertyWrapper
 public class State<Value> {
+    private var _originalValue: Value
     private var _wrappedValue: Value
     public var wrappedValue: Value {
         get { _wrappedValue }
         set {
+            let oldValue = _wrappedValue
             _wrappedValue = newValue
-            listeners.forEach { $0(newValue) }
+            listeners.forEach { $0(oldValue, newValue) }
         }
     }
     
@@ -14,14 +16,26 @@ public class State<Value> {
     }
 
     public init(initialValue value: Value) {
+        _originalValue = value
         _wrappedValue = value
     }
     
-    public typealias Listener = (Value) -> Void
+    public func reset() {
+        let oldValue = _wrappedValue
+        _wrappedValue = _originalValue
+        listeners.forEach { $0(oldValue, _wrappedValue) }
+    }
+    
+    public typealias Listener = (_ old: Value, _ new: Value) -> Void
+    public typealias SimpleListener = (_ value: Value) -> Void
     
     private var listeners: [Listener] = []
     
     public func listen(_ listener: @escaping Listener) {
         listeners.append(listener)
+    }
+    
+    public func listen(_ listener: @escaping SimpleListener) {
+        listeners.append({ _, new in listener(new) })
     }
 }
