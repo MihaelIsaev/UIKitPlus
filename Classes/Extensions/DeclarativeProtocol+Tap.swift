c4356257275b0c68e400bc28f5743e914b1c4046
Mihael Isaev <isaev.mihael@gmail.com>
2019-10-25 13:12:19 +0400

    Improve `TapGestureRecognizer`

--- Classes/Extensions/DeclarativeProtocol+Tap.swift
+++ Classes/Extensions/DeclarativeProtocol+Tap.swift
@@ -1,16 +1,24 @@
 import UIKit
 
 extension DeclarativeProtocol where V: UIView {
+    @available(*, deprecated, renamed: "onTapGesture")
     @discardableResult
-    public func tapAction(_ action: @escaping () -> Void) -> Self {
-        declarativeView.addGestureRecognizer(TapGestureRecognizer(action))
+    public func tapAction(_ action: @escaping () -> Void) -> Self { onTapGesture(action) }
+    
+    @available(*, deprecated, renamed: "onTapGesture")
+    @discardableResult
+    public func tapAction(_ action: @escaping (Self) -> Void) -> Self { onTapGesture(action) }
+    
+    @discardableResult
+    public func onTapGesture(count: Int = 1, _ action: @escaping () -> Void) -> Self {
+        declarativeView.addGestureRecognizer(TapGestureRecognizer(count: count, action))
         return self
     }
     
     @discardableResult
-    public func tapAction(_ action: @escaping (V) -> Void) -> Self {
-        let recognizer = TapGestureRecognizer { [weak self] in
-            guard let self = self as? V else { return }
+    public func onTapGesture(count: Int = 1, _ action: @escaping (Self) -> Void) -> Self {
+        let recognizer = TapGestureRecognizer(count: count) { [weak self] in
+            guard let self = self else { return }
             action(self)
         }
         declarativeView.addGestureRecognizer(recognizer)
@@ -19,14 +27,22 @@ extension DeclarativeProtocol where V: UIView {
 }
 
 extension DeclarativeProtocol where V: UIControl {
+    @available(*, deprecated, renamed: "onTapGesture")
     @discardableResult
-    public func tapAction(_ action: @escaping () -> Void, _ event: UIControl.Event = .touchUpInside) -> Self {
+    public func tapAction(_ action: @escaping () -> Void, _ event: UIControl.Event = .touchUpInside) -> Self { onTapGesture(event, action) }
+    
+    @available(*, deprecated, renamed: "onTapGesture")
+    @discardableResult
+    public func tapAction(_ action: @escaping (V) -> Void, _ event: UIControl.Event = .touchUpInside) -> Self { onTapGesture(event, action) }
+        
+    @discardableResult
+    public func onTapGesture(_ event: UIControl.Event = .touchUpInside, _ action: @escaping () -> Void) -> Self {
         declarativeView.actionHandler(controlEvents: event, forAction: action)
         return self
     }
     
     @discardableResult
-    public func tapAction(_ action: @escaping (V) -> Void, _ event: UIControl.Event = .touchUpInside) -> Self {
+    public func onTapGesture(_ event: UIControl.Event = .touchUpInside, _ action: @escaping (V) -> Void) -> Self {
         declarativeView.actionHandler(controlEvents: event) { [weak self] in
             guard let self = self as? V else { return }
             action(self)
--- Classes/Objects/TapGestureRecognizer.swift
+++ Classes/Objects/TapGestureRecognizer.swift
@@ -3,9 +3,10 @@ import UIKit
 final class TapGestureRecognizer: UITapGestureRecognizer {
     var action: () -> Void
 
-    init(_ action: @escaping () -> Void) {
+    init(count: Int = 1, _ action: @escaping () -> Void) {
         self.action = action
         super.init(target: nil, action: nil)
+        numberOfTapsRequired = count
         addTarget(self, action: #selector(execute))
     }
 
