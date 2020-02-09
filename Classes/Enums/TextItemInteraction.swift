22522c94fe6168a0f0d46343df3ab4d3a3f22042
Mihael Isaev <isaev.mihael@gmail.com>
2020-02-09 16:59:39 +0400

    Implement TextItemInteraction

---
+++ Classes/Enums/TextItemInteraction.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+public enum TextItemInteraction : Int {
+    case invokeDefaultAction, presentActions, preview
+    
+    @available(iOS 10.0, *)
+    static func from(_ item: UITextItemInteraction) -> TextItemInteraction {
+        switch item {
+        case .invokeDefaultAction: return .invokeDefaultAction
+        case .presentActions: return .presentActions
+        case .preview: return .preview
+        }
+    }
+}
