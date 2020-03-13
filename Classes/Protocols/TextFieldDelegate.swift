4873577ec93e3a8b690e269b50752280c418472d
Mihael Isaev <isaev.mihael@gmail.com>
2020-03-13 15:32:52 +0400

    Extract `TextFieldDelegate` into separate file

---
+++ Classes/Protocols/TextFieldDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  TextFieldDelegate.swift
+//  UIKit-Plus
+//
+//  Created by Mihael Isaev on 13.03.2020.
+//
+
+import Foundation
--- Classes/Views/TextFieldView.swift
+++ Classes/Views/TextFieldView.swift
@@ -559,33 +559,6 @@ open class TextField: UITextField, UITextFieldDelegate, DeclarativeProtocol, Dec
     }
 }
 
-@objc
-public protocol TextFieldDelegate: NSObjectProtocol {
-    @objc @available(iOS 2.0, *)
-    optional func textFieldShouldBeginEditing(_ textField: TextField) -> Bool
-    
-    @objc @available(iOS 2.0, *)
-    optional func textFieldDidBeginEditing(_ textField: TextField)
-    
-    @objc @available(iOS 2.0, *)
-    optional func textFieldShouldEndEditing(_ textField: TextField) -> Bool
-    
-    @objc @available(iOS 2.0, *)
-    optional func textFieldDidEndEditing(_ textField: TextField)
-    
-    @objc @available(iOS 10.0, *)
-    optional func textFieldDidEndEditing(_ textField: TextField, reason: TextField.DidEndEditingReason)
-    
-    @objc @available(iOS 2.0, *)
-    optional func textField(_ textField: TextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
-    
-    @objc @available(iOS 2.0, *)
-    optional func textFieldShouldClear(_ textField: TextField) -> Bool
-    
-    @objc @available(iOS 2.0, *)
-    optional func textFieldShouldReturn(_ textField: TextField) -> Bool
-}
-
 extension TextField: Refreshable {
     /// Refreshes using `RefreshHandler`
     public func refresh() {
