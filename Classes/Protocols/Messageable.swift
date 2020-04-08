a1c7cf051b7ae0ca4d496431857cd7d687f2fe5e
Mihael Isaev <isaev.mihael@gmail.com>
2020-04-09 00:27:16 +0400

    Fix Messageable protocol

--- Classes/Protocols/Messageable.swift
+++ Classes/Protocols/Messageable.swift
@@ -18,7 +18,20 @@ protocol _Messageable: Messageable {
     func _setMessage(_ v: String?)
 }
 
-extension Titleable {
+extension Messageable {
+    @discardableResult
+    public func message<V>(_ expressable: ExpressableState<V, String>) -> Self {
+        message(expressable.unwrap())
+    }
+
+    @discardableResult
+    public func message(@StateStringBuilder stateString: @escaping StateStringBuilder.Handler) -> Self {
+        message(stateString())
+    }
+}
+
+@available(iOS 13.0, *)
+extension Messageable {
     @discardableResult
     public func message(_ text: String) -> Self {
         guard let s = self as? _Messageable else { return self }
@@ -33,14 +46,20 @@ extension Titleable {
         state.listen { s._setMessage($0) }
         return self
     }
+}
 
+// for iOS lower than 13
+extension _Messageable {
     @discardableResult
-    public func message<V>(_ expressable: ExpressableState<V, String>) -> Self {
-        message(expressable.unwrap())
+    public func message(_ text: String) -> Self {
+        _setMessage(text)
+        return self
     }
 
     @discardableResult
-    public func message(@StateStringBuilder stateString: @escaping StateStringBuilder.Handler) -> Self {
-        message(stateString())
+    public func message(_ state: State<String>) -> Self {
+        _setMessage(state.wrappedValue)
+        state.listen { self._setMessage($0) }
+        return self
     }
 }
