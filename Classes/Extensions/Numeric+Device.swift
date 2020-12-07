5d0820540cf3775be714d6b5aac1aa5f26683b9c
Mihael Isaev <isaev.mihael@gmail.com>
2020-12-08 03:44:00 +0400

    📱 Numeric+Device upgrade: add new devices

--- Classes/Extensions/Numeric+Device.swift
+++ Classes/Extensions/Numeric+Device.swift
@@ -16,25 +16,66 @@ public func !! <T>(lhs: T, rhs: iPhoneNumeric) -> T where T: BinaryFloatingPoint
 }
 
 public enum iPhoneNumeric {
+    /// Same as 4S
     case iPhone4(Any)
+    
+    /// Same as 5S and old SE
     case iPhone5(Any)
+    
+    /// Same as 7, 8, new SE
     case iPhone6(Any)
+    
+    /// Same as 7+, 8+
     case iPhone6Plus(Any)
+    
+    /// Same as 6, 8, new SE
     case iPhone7(Any)
+    
+    /// Same as 6+, 8+
     case iPhone7Plus(Any)
+    
+    /// Same as 6, 7, new SE
     case iPhone8(Any)
+    
+    /// Same as 6+, 7+
     case iPhone8Plus(Any)
+    
+    /// Same as 11
     case iPhoneXr(Any)
+    
+    /// Same as XS, Mini
     case iPhoneX(Any)
+    
+    /// Same as XSMax, ProMax
     case iPhoneXMax(Any)
+    
+    /// Same as X, Mini
     case iPhoneXS(Any)
+    
+    /// Same as XMax, ProMax
     case iPhoneXSMax(Any)
+    
+    /// Same as Xr
+    case iPhone11(Any)
+    
+    /// Same as X, XS
     case iPhoneMini(Any)
+    
+    /// Unique screen size at the moment
     case iPhonePro(Any)
+    
+    /// Same as XMax, XSMax
     case iPhoneProMax(Any)
+    
+    /// Classic 9" iPad or iPad mini
     case iPad(Any)
+    
+    /// 10" iPad
     case iPad10(Any)
+    
+    /// 12" iPad
     case iPad12(Any)
+    
     var isActual: Bool {
         switch self {
         case .iPhone4: return UIDevice.isPhone4
@@ -50,6 +91,7 @@ public enum iPhoneNumeric {
         case .iPhoneXMax: return UIDevice.isPhoneXMax
         case .iPhoneXS: return UIDevice.isPhoneX
         case .iPhoneXSMax: return UIDevice.isPhoneXMax
+        case .iPhone11: return UIDevice.isPhoneXr
         case .iPhoneMini: return UIDevice.isPhoneX
         case .iPhonePro: return UIDevice.isPhonePro
         case .iPhoneProMax: return UIDevice.isPhoneXMax
@@ -101,6 +143,9 @@ extension BinaryInteger {
             return Self(Double(String(describing: v)) ?? Double(self))
         case .iPhoneXSMax(let v):
             guard UIDevice.isPhoneXMax else { return self }
+            return Self(Double(String(describing: v)) ?? Double(self))
+        case .iPhone11(let v):
+            guard UIDevice.isPhoneXr else { return self }
             return Self(Double(String(describing: v)) ?? Double(self))
         case .iPhoneMini(let v):
             guard UIDevice.isPhoneX else { return self }
