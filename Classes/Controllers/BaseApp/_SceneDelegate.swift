be82d553352bd16204b3892dd1c607098f0b8671
Anton Lobanov <alobanov11@gmail.com>
2021-02-27 16:46:50 +0300

    Fix method 'application:openURL:options:' is not called
    https://stackoverflow.com/questions/58624786/method-applicationopenurloptions-is-not-called

--- Classes/Controllers/BaseApp/_SceneDelegate.swift
+++ Classes/Controllers/BaseApp/_SceneDelegate.swift
@@ -53,6 +53,17 @@ class _SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
+
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else {
+            return
+        }
+        let _ = BaseApp.shared.lifecycle?._openURLSourceAppAnnotation?(
+            url,
+            nil,
+            [UIApplication.OpenURLOptionsKey.annotation]
+        )
+    }
     
     func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
         BaseApp.shared.shortcuts
