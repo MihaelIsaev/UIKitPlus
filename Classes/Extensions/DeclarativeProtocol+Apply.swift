c205d1cad88fbb17dbf198727c7af30818423217
cielpy <beijiu572@gmail.com>
2022-01-03 23:43:09 +0800

    fixed code review issues.

--- Classes/Extensions/DeclarativeProtocol+Apply.swift
+++ Classes/Extensions/DeclarativeProtocol+Apply.swift
@@ -2,9 +2,14 @@
 // Created by Kevin Guo on 2022/1/3.
 //
 
-import Foundation
 extension DeclarativeProtocol {
-
+    /// Helper method where you could change the object non-declarative way
+    ///
+    /// ```swift
+    /// view1.apply {
+    ///     $0.backgroundColor = .red
+    /// }
+    /// ```
     @discardableResult
     public func apply(_ closure: (V) -> Void) -> Self {
         closure(declarativeView)
